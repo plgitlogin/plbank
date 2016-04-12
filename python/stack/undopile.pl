1a3f9b0a5dbc594b9f736965d00eb8b8e9d11e91
revuz <dominique.revuz@gmail.com>
2016-04-12 12:45:40 +0200

    work

--- python/stack/undopile.pl
+++ python/stack/undopile.pl
@@ -3,15 +3,15 @@
 author=Dominique Revuz 
 title=undopile.pl
 tagname=stack
-files=@mydatafile
-files=@secbde
 text==
 
 un collègue programmeur écrit un éditeur, pour pouvoir proposer un mécanisme de **Undo** (annulation), il a besoin de pouvoir stocker des commandes et de pouvoir récuperer ces commandes. Bien sur il veut récupérer les commandes dans l'ordre inverse dans lesquelles il les a entrées. Comme ces commandes peuvent prendre beaucoup de place nous souhaitons limiter le nombre de undo possible à TAILLE (valeur par default 100). 
 
-Pour lui simplifier la vie je vais lui proposer trois fonctions: **setTaille(taille)** qui modifie le nombre maximal de commande enregistrées.
+Pour lui simplifier la vie je vais lui proposer trois fonctions: **setTaille(pile,taille)** qui modifie le nombre maximal de commande enregistrées.
 **savecommande(commande)** qui sauvegarde une commande (et oublie le plus vielle commande si l'ajout fait dépasser la taille maximum.
-**loadcommande(commande)** qui dépile la dernière commande et renvoi celle ci.
+**loadcommande()** qui dépile la dernière commande et renvoi celle ci.
+
+Pour stocker la pile nous utiliserons une liste que l'on passe en paramêtre, la position 0 contenant la taille maximale. 
 
 ==
 code==
@@ -29,6 +29,11 @@ dico_bad = { "success": False , "errormessages" : "création d'une exception", "
 
 try:
 	import student
+	mapile = []
+	savecommande(mapile,"open") 
+	if loadcommande(mapile) != "open" :
+		dico_bad["error messages"]= "Ne retourne pas l'élément entrée"
+		print(json.dumps(dico_bad))
 	print(json.dumps(dico_good))
 except:
     print(json.dumps(dico_bad))
