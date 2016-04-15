bceee8330948e61b2a3440aca4fd1be0985e1c9b
revuz <dominique.revuz@gmail.com>
2016-04-15 14:25:31 +0200

    testrechargementquestionbank

--- python/exemple/exemple1.pl
+++ python/exemple/exemple1.pl
@@ -7,6 +7,10 @@ Veuiller ecrire un code qui ne lève pas dexception
 
 Ceci est l'ENNONCEEE QUI N'APPARAIT PAS 
 
+version pour tester le rechargement après avoir efface un exercice dans une question bank
+si elle est encore visible pour la page pltp ou si c'est la nouvelle valeur qui 
+est prise en compte ou si il y a une erreur.
+
 ==
 code==
 # Veuillez saisir votre code ici
