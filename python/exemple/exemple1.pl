b8271e964d025de478468ab7f6b6e0552a05b14f
revuz <dominique.revuz@gmail.com>
2016-05-18 14:00:56 +0200

    work

--- python/exemple/exemple1.pl
+++ python/exemple/exemple1.pl
@@ -1,20 +1,28 @@
-
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
+# la ligne suivante est nécessaire pour faire appel à la fonction grade.
+template=/python/function/functiongradertemplate.pl
+
 title=exemple1.pl
 text==
-Veuiller ecrire un code qui ne lève pas dexception 
+Ecrire deux lignes de code permettant d'initialiser les variables //hauteur// avec 1.33 et //largeur// avec 2.2 .
 
-Ceci est l'ENNONCEEE QUI N'APPARAIT PAS 
-
-version pour tester le rechargement après avoir efface un exercice dans une question bank
-si elle est encore visible pour la page pltp ou si c'est la nouvelle valeur qui 
-est prise en compte ou si il y a une erreur.
 
 ==
 code==
 # Veuillez saisir votre code ici
-
 ==
-grader=@mongarder.py
-
+grader==
+# la variable suivante contient le code de test a réaliser pour vérifier le code 
+# la syntaxe est celle de doctest https://docs.python.org/3.5/library/doctest.html
+__doc__="import student # le code a tester 
+>>>hauteur
+1.33 # le premier test 
+>>>largeur
+2.2
+" # fin de la définition du test 
+# import de la fonction d'évaluation 
+from functiongrader import grade
+# réalisation du test et création du json nécessaire comme sortie du l'executeur 
+grade()
+==
