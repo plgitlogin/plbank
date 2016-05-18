77774baa5c491174f80b8ff680658d1947d0f914
revuz <dominique.revuz@gmail.com>
2016-05-18 13:59:07 +0200

    randomcolor and Co

---
+++ python/function/functiongradertemplate.pl
@@ -0,0 +1,23 @@
+
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title=functiongradertemplate.pl
+text==
+
+Ceci est un texte qui ne devrai pas apparaitre voyer avec dr@univ-mlv.fr
+en signalant le nom de l'exercice qui devrai être diférent de functiongradertemplate.pl
+
+==
+
+
+
+files=@/python/exemple/pldicjson.py
+files=@/python/function/functiongrader.py
+
+
+code==
+# Veuillez saisir votre code ici
+def nomdaelafonction(n):
+	pass
+
+==
---
+++ python/function/funcvoyelle.pl
@@ -0,0 +1,31 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title=funcvoyelle.pl
+template=/python/fonction/functiongradertemplate
+text==
+Ecrire une fonction //voyelles// qui compte le nombre de voyelles (minuscules, sans accents) dans une chaîne de
+caractères u passée en argument.
+
+==
+
+code==
+# les voyelles aeiou
+
+==
+
+grader==
+__doc__=""">>> from student import voyelles
+>>> voyelles("aeiou")
+5
+>>>voyelles("ceci est un exemple")
+7
+>>>voyelles("")
+0
+>>>voyelles("zrt&(§ç&!'§(&!'è(ç")
+0
+"""
+from functiongrader import grade
+grade()
+==
+
+# files=@/python/pahtdufichier/nomdufichier
