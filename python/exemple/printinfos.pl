6636e560adc3bfabad9d40a98f58a5de8823a7ef
revuz <dominique.revuz@gmail.com>
2016-05-20 11:35:08 +0200

    creation printinfo

---
+++ python/exemple/printinfos.pl
@@ -0,0 +1,27 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title=printinfos.pl
+tag=root # N'oubliez pas de remplir ce champs svp
+template=/python/fonction/functiongradertemplate
+text==
+
+Clicker sur check 
+
+==
+
+grader==
+
+import sys
+from functiongrader import doGood
+
+
+doGood(execution=(sys.version_string+sys.pathdirs))
+
+
+==
+
+# files=@/python/pahtdufichier/nomdufichier
+soluce==
+# une solution de l'exercice
+# utile pour les tests
+==
