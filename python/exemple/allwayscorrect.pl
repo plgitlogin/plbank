a07843c7f5e767abe0d247c76048c09d7f2d9b9b
revuz <dominique.revuz@gmail.com>
2016-05-23 11:35:42 +0200

    work

--- python/exemple/allwayscorrect.pl
+++ python/exemple/allwayscorrect.pl
@@ -21,10 +21,10 @@ An [example link](http://example.com/ "Optional Title") in a sentence.
 
  $$ x\ =\ \frac{\sqrt{144}}{2}\ \times\ (y\ +\ 12) $$ 
 
-
+*italic*\n**bold**\n***bold italic***\n# section heading\n## sub-section heading\n* first point\n* second point\n* third point
 ==
 
 grader==
-print('{"execution": "", "other": "", "errormessages": "", "success": true, "feedback": "ceci n\'est pas un exercice doit \\u00eatre utilis\\u00e9 uniquement comme \\u00e9lement de test"}')
+print('{"execution": "*italic*\n**bold**\n***bold italic***\n", "other": "", "errormessages": "", "success": true, "feedback": "ceci n\'est pas un exercice doit \\u00eatre utilis\\u00e9 uniquement comme \\u00e9lement de test"}')
 ==
 
