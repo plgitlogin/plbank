a48ce9e923532425075645e7d9e7d7ac552bf077
revuz <dominique.revuz@gmail.com>
2016-05-23 11:26:57 +0200

    work

--- python/exemple/allwayscorrect.pl
+++ python/exemple/allwayscorrect.pl
@@ -1,7 +1,6 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz
-template=/python/function/functiongradertemplate.pl
-title=allwayscorrect.pl
+title=Toujours Correct
 tag=test # N'oubliez pas de remplir ce champs svp
 
 text==
@@ -20,11 +19,12 @@ text==
 
 An [example link](http://example.com/ "Optional Title") in a sentence.
 
+ $$ x\ =\ \frac{\sqrt{144}}{2}\ \times\ (y\ +\ 12) $$ 
+
+
 ==
 
 grader==
-import pldoctest
-from functiongrader import doGood
-doGood(feedback="ceci n'est pas un exercice doit être utilisé uniquement comme élement de test")
+print({ "success": True , "errormessages" : "" , "execution": "OK", "feedback": "ceci n'est pas un exercice doit être utilisé uniquement comme élement de test", "other": "" })
 ==
 
