877dee4045de2b9cc702d9dce51780479cf1d765
revuz <dominique.revuz@gmail.com>
2016-05-23 15:04:48 +0200

    work

--- python/exemple/testFormat.pl
+++ python/exemple/testFormat.pl
@@ -21,10 +21,41 @@ An [example link](http://example.com/ "Optional Title") in a sentence.
 
  $$ x\ =\ \frac{\sqrt{144}}{2}\ \times\ (y\ +\ 12) $$ 
 
-*italic*\n**bold**\n***bold italic***\n# section heading\n## sub-section heading\n* first point\n* second point\n* third point
 ==
 
 grader==
-print('{"execution": "*italic* **bold** ***bold italic***", "other": "", "errormessages": "", "success": true, "feedback": "ceci n\'est pas un exercice doit \\u00eatre utilis\\u00e9 uniquement comme \\u00e9lement de test"}')
+import json
+dico_good = { "success": True , "errormessages" : "" , "execution": "OK", "feedback": "ok", "other": "" }
+
+
+def doGood(success=True,error="",execution="OK",feedback="Bravo",other=""):
+	dico_good["success"]=success
+	dico_good["error"]=error
+	dico_good["execution"]=execution
+	dico_good["feedback"]=feedback
+	dico_good["other"]=other
+	print(json.dumps(dico_good))
+
+sortie="""
+*italic*
+**bold**
+***bold italic***
+
+# section heading
+## sub-section heading
+### sub-sub-section heading
+
+* first point
+* second point
+* third point
+
+An [example link](http://example.com/ "Optional Title") in a sentence.
+
+ $$ x\ =\ \frac{\sqrt{144}}{2}\ \times\ (y\ +\ 12) $$ 
+
+
+"""
+doGood(execution=sortie)
+
 ==
 
