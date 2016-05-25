474f8fd761ad07d56d9f9194fe7c8be13b1548fa
revuz <dominique.revuz@gmail.com>
2016-05-25 11:41:13 +0200

    work

--- python/function/simplefunc1.pl
+++ python/function/simplefunc1.pl
@@ -1,9 +1,12 @@
 template=/python/function/functiongradertemplate
-title=simplefunc1.pl
+title=Fizz Buzz 
+
 text==
 Veuiller ecrire un code qui défini une fonction //fizzbuzz(n)// qui prend en parameters un entier
 et qui affiche Fizz si n est divisible par 3 et  Buzz si n est divisible par 7.
 ==
+
+
 grader==
 __doc__=""">>> from student import fizzbuzz
 >>> fizzbuzz(3) # divisible par 3
@@ -19,21 +22,4 @@ __doc__=""">>> from student import fizzbuzz
 """
 from functiongrader import grade
 grade()
-==
-
-
-
-letest==
-__doc__=""">>> from student import fizzbuzz
->>> fizzbuzz(3) # divisible par 3
-'Fizz'
->>> fizzbuzz(7) # divisible par 7
-'Buzz'
->>> fizzbuzz(3*7) # divisible par les deux 
-'Fizz Buzz'
->>> fizzbuzz(3*7*3841) # divisible par les deux 
-'Fizz Buzz'
->>> fizzbuzz(11) # ni l'un ni l'autre 
->>> 
-"""
 ==
