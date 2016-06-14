aa2c9ee0c68c9a344869b90eb3ffd0a76d94b2f6
revuz <dominique.revuz@gmail.com>
2016-06-14 10:55:36 +0200

    work

--- python/exemple/autograder.py
+++ python/exemple/autograder.py
@@ -5,22 +5,6 @@
 #  
 #  Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 #  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
 
 
@@ -28,6 +12,7 @@ import sys
 import json 
 import pldoctest
 import io
+from pldicjson import getpldic
 
 __pl__="""
 	Utilise la balise pltest comme élément de test 
@@ -47,7 +32,7 @@ def doGood(success=True,error="",execution="OK",feedback=None,other=""):
 		dico_reponse["feedback"]=feedback
 	else:
 		dicjson = getpldic()
-		if dicjson["feedback"]:
+		if "feedback" in dicjson:
 			dico_reponse["feedback"] = dicjson["feedback"]
 		else:
 			dico_reponse["feedback"] = ""
@@ -66,6 +51,36 @@ def doBad(success=False,error="Des erreurs dans l'exécution",execution="pas de 
 	dico_reponse["other"]=other
 	dico_reponse["errormessages"] = errormessages
 	print(json.dumps(dico_reponse)) 
+
+def __gg__(l):
+	for u in l:
+			yield str(u)
+
+
+import builtins
+def mockinput(thelist):
+	bob = __gg__(thelist)
+	builtins.input = lambda prompt="toto":  str(next(bob))
+
+from importlib import import_module, reload 
+student=None
+
+def doloadstudent():
+	global student
+	if student == None:
+		student = import_module("student")
+	else:
+		reload(student)
+
+
+def dostudent(l):
+		mockinput(l)
+		doloadstudent()
+
+
+
+
+
 
 
 def compiletest():
