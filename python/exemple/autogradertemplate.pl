3b417aa5a97d7bc06a391c990cd76903e2eac441
revuz <dominique.revuz@gmail.com>
2016-06-15 17:14:08 +0200

    work

--- python/exemple/autogradertemplate.pl
+++ python/exemple/autogradertemplate.pl
@@ -1,7 +1,7 @@
 
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
-title= autofunctiongradertemplate.pl
+title= python/exemple/autofunctiongradertemplate.pl
 text==
 
 Ceci est un texte qui ne devrai pas apparaitre voyez avec dr@univ-mlv.fr
