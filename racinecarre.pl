1e10e55b593095fb700e408902c0af0f4e79c1b7
Dominique Revuz <dominique.revuz@univ-mlv.fr>
2016-06-17 11:43:22 +0200

    work

---
+++ racinecarre.pl
@@ -0,0 +1,30 @@
+# ceci est un exercice Premier langage
+
+template=/python/exemple/autogradertemplate
+text==
+
+Ecrire une fonction **racinecarre** qui calcul la racine carré de son paramètre.
+
+Deux posibilité soit vous allez chercher dans une librairie une fonction **sqrt** (square root) qui vous utilisez.
+Soit vous savez que la racine carré de n est n à l'exposant 1/2 et que l'opérateur de puissance en python est \*\* (double étoile).
+
+==
+code==
+# Votre code
+==
+
+
+pltest=
+>>> racinecarre(1234)
+35.12833614050059
+>>> racinecarre(1000000)
+1000.0
+>>> racinecarre(10000000)
+3162.2776601683795
+>>> racinecarre(10**100)
+1e+50
+>>> racinecarre(0)
+0.0
+>>>
+==
+
