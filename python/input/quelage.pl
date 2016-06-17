dd7045a122e84eb76754df2b330b0cb4286fb824
revuz <dominique.revuz@gmail.com>
2016-06-17 22:00:08 +0200

    work

---
+++ python/input/quelage.pl
@@ -0,0 +1,31 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title= Quel age à le capitaine
+tag=input|print|variable
+template=/python/exemple/autogradertemplate
+
+text==
+Demandez au Capitaine son année de naissance et calculer et afficher son age au 31 décembre 2018 à Minuit.
+== 
+
+code==
+# 
+age=int(input())
+# Le capitaine auras XXX ans.
+
+==
+
+feedback==
+Ok top passons à quelque chose de plus dur ;). 
+==
+
+inputgenerator==
+from random import randint 
+print(randint(10,78)+1939)
+
+==
+
+soluce==
+a=int(input())
+print("Le capitaine auras %d ans." % (2018-a))
+==
