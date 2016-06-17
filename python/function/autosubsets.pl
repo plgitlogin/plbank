ffc74024a18615393e06d5891990bef7d72d0ac4
revuz <dominique.revuz@gmail.com>
2016-06-17 16:03:42 +0200

    work

---
+++ python/function/autosubsets.pl
@@ -0,0 +1,25 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title=subsets.pl
+tag=root # N'oubliez pas de remplir ce champs svp
+template=/python/function/autogradertemplate
+text==
+Ecrire une fonction *subsets* qui retourne l'ensemble des sous ensembles de l'ensemble passé en parametre.
+
+==
+
+pltest==
+>>> from student import subsets
+>>> subsets(set([]))
+[[]]
+>>> subsets({"Toto","titi"})
+[[], ['titi'], ['Toto'], ['titi', 'Toto']]
+>>> subsets(set([1,2,3,4,5])
+[[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3], [4], [1, 4], [2, 4], [1, 2, 4], [3, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
+>>> 
+==
+
+feedback==
+une version "OneLiner"<br/>
+subsets = lambda x: [[y for j, y in enumerate(set(x)) if (i >> j) & 1] for i in range(2**len(set(x)))]
+==
