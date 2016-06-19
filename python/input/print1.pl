2c564833c2e392fe7a2557ff58ab8f46515fb057
revuz <dominique.revuz@gmail.com>
2016-06-19 22:04:07 +0200

    work

---
+++ python/input/print1.pl
@@ -0,0 +1,32 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name=print1.pl
+title=La fonction print
+tag=print
+template=/python/exemple/autogradertemplate
+text==
+
+La fonction **print** permet de faire des affichages (envois de caractère à l'écran).
+
+Nous allons l'utiliser pour écrire votre premier programme python.
+
+Ecrivez une ligne de code de la forme :
+
+‘‘‘python
+print(votreprénomentreguillemets)
+‘‘‘
+
+==
+
+# Choisir pltest ou soluce ou expectedoutput
+
+pltest==
+>>> print("ok")
+ok
+==
+
+feedback==
+Très bien a chaque fois que vous souhaitez avoir une information pendant l'execution de votre programme il suffit de faire un **print** de 
+cette information.<br>
+Remarque en python 2 les paranthèses ne sont pas nécessaire, mais garder l'habitude de les utiliser.
+==
