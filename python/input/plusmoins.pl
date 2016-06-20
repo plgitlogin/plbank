f7d752a5e471a8f13c5785be57968e2204fb38dc
revuz <dominique.revuz@gmail.com>
2016-06-20 18:11:38 +0200

    work

--- python/input/plusmoins.pl
+++ python/input/plusmoins.pl
@@ -5,12 +5,15 @@ tag=input|print
 template=/python/exemple/autogradertemplate
 
 text==
-Ecrire un programme qui demande à l’utilisateur de saisir quatre entiers au clavier (un par ligne), affiche ensuite une valeur par ligne, puis leur somme et leur moyenne sur la ligne suivante. Par exemple, si l’utilisateur entre les valeurs 12, 3, 14 et 5, le programme affiche :
-12
-3
-14
-5
-somme= 34 , moyenne = 8.5
+Ecrire un programme qui demande à l’utilisateur de saisir quatre entiers au clavier (un par ligne), affiche ensuite une valeur par ligne, puis leur somme et leur moyenne sur la ligne suivante. Par exemple, si l’utilisateur entre les valeurs
+	12, 3, 14 et 5,
+le programme affiche :
+	
+	12
+	3
+	14
+	5
+	somme= 34 , moyenne = 8.5
 == 
 
 feedback==
