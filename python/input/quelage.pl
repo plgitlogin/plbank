6f142644501a98b019c506798d69389d299e912f
revuz <dominique.revuz@gmail.com>
2016-06-20 17:52:31 +0200

    work

--- python/input/quelage.pl
+++ python/input/quelage.pl
@@ -3,7 +3,7 @@ author=Dominique Revuz
 title= Quel age à le capitaine
 tag=input|print|variable
 template=/python/exemple/autogradertemplate
-
+name=Capitaine
 text==
 Demandez au Capitaine son année de naissance puis calculez et affichez son age au 31 décembre 2018 à Minuit.
 == 
