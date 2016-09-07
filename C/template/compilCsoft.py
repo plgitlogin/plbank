2e2551df1889cabc138eecb20eacf91a7166234a
Nicolas Borie <nicolas.borie@u-pem.fr>
2016-09-07 17:44:06 +0200

    Rafinage du grader C partie compilation

--- C/template/compilCsoft.py
+++ C/template/compilCsoft.py
@@ -20,17 +20,16 @@ import json
 
 dico_reponse = { "success": True , "errormessages" : "" ,
                  "execution": "Plateforme Error", "feedback": "", 
-                 "other": "", "compilation" : "Erreur" }
+                 "other": "", "compilation" : "erreur" }
 
 def compiletest(ldflags=""):
     # Compilation command
     compilation_command = "gcc basic.c -o progCstudent "+ ldflags
 
-    # Ugly : two times the compilations and constitution of two log files
-    os.system(compilation_command + " 1> compilCstdout.log")
     os.system(compilation_command + " 2> compilCstderr.log")
     err_out_log = open("compilCstderr.log", "r")
     err_out = err_out_log.read()
+    err_out_log.close()
 
     # If there is some compilation errors
     if "error:" in err_out:
@@ -38,19 +37,16 @@ def compiletest(ldflags=""):
         dico_reponse["errormessages"] = "Voir le feedback donné par le compilateur gcc"
         dico_reponse["execution"] = "Impossible"
         dico_reponse["feedback"] = "Il y a des erreurs à la compilation de votre programme :\n" + err_out
-        err_out_log.close()
         return False
 
-    err_out_log.close()
-    std_out_log = open("compilCstdout.log", "r")
-    std_out = std_out_log.read()
-
+    # If there is some warnings
     if "warning:" in err_out:
         dico_reponse["feedback"] = "Vous pouvez augmenter la qualité de votre programme en lisant les recommandations du compilateur:\n" + std_out
-        dico_reponse["compilation"] = "Warnings"
+        dico_reponse["compilation"] = "warning"
+    # No error, no warning
     else:
         dico_reponse["feedback"] = "Votre programme semble être écrit correctement\n"
-        dico_reponse["compilation"] = "Parfaite"
+        dico_reponse["compilation"] = "parfaite"
     # Here, the compilation is OK with possible warnings
     std_out_log.close()
     return True
