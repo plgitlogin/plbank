90ae2434f09590a2837a4c2640b63bafc3dd566e
revuz <dominique.revuz@gmail.com>
2016-09-07 12:52:15 +0200

    creation du C

---
+++ C/printf/hello.pl
@@ -0,0 +1,36 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= Premier pas C
+title= 
+tag=  # N'oubliez pas de remplir ce champs svp
+template=/C/template/autogradertemplate
+text==
+
+Faites un Hello World 
+
+
+
+==
+
+
+code==
+#include <stdio.h>
+
+/* votre code SVP */
+int main(){
+
+}
+==
+
+soluce==
+#include <stdio.h>
+int main(){
+print("Hello World\n");
+}
+==
+
+
+
+expectedoutput==
+Hello World
+==
---
+++ C/template/basic.c
@@ -0,0 +1,15 @@
+/*
+ * basic.c
+ * 
+ * Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+ * 
+
+pour compiler le travail des eleves 
+
+ * 
+ */
+
+
+#include "student.py"
+
+
