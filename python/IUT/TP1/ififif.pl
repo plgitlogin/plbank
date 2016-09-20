66c6ff000743e256b69f8737836b8b5e50ca13be
revuz <dominique.revuz@gmail.com>
2016-09-20 18:30:00 +0200

    work

---
+++ python/IUT/TP1/ififif.pl
@@ -0,0 +1,69 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= Branchements
+title=  if if if and else
+tag=if|else|and  # N'oubliez pas de remplir ce champs svp
+template=/python/IUT/template
+text==
+
+Demander un entier à l'utilisateur avec //input// puis afficher si il c'est un multiple de 2 de 3  et de 7 avec les affichages suivants.
+<br>
+Pour 2 <br>
+Affiche :<br>
+	2 est divisible par 2.
+Pour 6 <br>
+	6 est divisible par 2 et par 3.
+Pour 42 <br>
+	42 est divisible par 2 et par 3 et par 7.
+Pour 17 <br>
+	17 n'est divisble ni par 2 ni par 3 ni par 7.
+
+Faites l'exercice sans utiliser les opérateurs and et or.
+
+==
+
+# Choisir pltest ou soluce ou expectedoutput
+
+soluce==
+X=input("Entrez un entier")
+if X % 2 == 0 and X % 3 == 0 and X % 7 ==0 :
+	print("%d est divisible par 2 et par 3 et par 7." % X)
+if X % 2 == 0 and X % 3 != 0 and X % 7 ==0 :
+	print("%d est divisible par 2 et par 7." % X)
+if X % 2 == 0 and X % 3 == 0 and X % 7 !=0 :
+	print("%d est divisible par 2 et par 3." % X)
+if X % 2 != 0 and X % 3 == 0 and X % 7 ==0 :
+	print("%d est divisible par 3 et par 7." % X)
+if X % 2 != 0 and X % 3 == 0 and X % 7 !=0 :
+	print("%d est divisible par 3." % X)
+if X % 2 == 0 and X % 3 != 0 and X % 7 !=0 :
+	print("%d est divisible par 2." % X)
+if X % 2 != 0 and X % 3 != 0 and X % 7 ==0 :
+	print("%d est divisible par 7." % X)
+if X % 2 != 0 and X % 3 != 0 and X % 7 !=0 :
+	print("%d n'est divisble ni par 2 ni par 3 ni par 7." % X)
+==
+
+input-0==
+2
+==
+input-1==
+6
+==
+input-2==
+42
+==
+input-3==
+17
+==
+input-4==
+14
+==
+input-5==
+77
+==
+input-6==
+69
+==
+
+taboo=and|or
