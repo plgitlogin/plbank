9a611e0db736fe4606c9f6c784ca5cf1d6110450
revuz <dominique.revuz@gmail.com>
2016-09-20 16:35:04 +0200

    work

--- python/exemple/debugtrue.pl
+++ python/exemple/debugtrue.pl
@@ -1,7 +1,7 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
-name=debugtrue.pl
-title=  Debug False erreur # N'oubliez pas de remplir ce champs svp
+name=YYYYYYYY.pl
+title=  Debug True erreur # N'oubliez pas de remplir ce champs svp
 tag=  # N'oubliez pas de remplir ce champs svp
 template=/python/exemple/autogradertemplate
 text==
@@ -11,6 +11,6 @@ Exercice de Debug de la sortie
 ==
 
 
-debug=False
+debug=True
 # Choisir pltest ou soluce ou expectedoutput
 
