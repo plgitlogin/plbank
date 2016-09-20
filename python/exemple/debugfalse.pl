4c6395091135b738e61b9be41f214faa7613797b
revuz <dominique.revuz@gmail.com>
2016-09-20 16:00:45 +0200

    work

--- python/exemple/debugfalse.pl
+++ python/exemple/debugfalse.pl
@@ -1,7 +1,7 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
 name=debugtrue.pl
-title=   # N'oubliez pas de remplir ce champs svp
+title=  Debug du success affichage des différents champs # N'oubliez pas de remplir ce champs svp
 tag=  # N'oubliez pas de remplir ce champs svp
 template=/python/exemple/autogradertemplate
 text==
