b7a017ea43da1a207ba09b399a96db9839c1ae7f
Dominique <dominique.revuz@gmail.com>
2016-10-02 19:14:30 +0200

    work

---
+++ python/IUT/TP1/operator001.pl
@@ -0,0 +1,43 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= Boites d'oeufs
+title= Modulo et Diviser
+tag= print|input|operator.mod|operator.floordiv
+template=/python/IUT/template
+text==
+
+# Operator  // et % 
+
+Joelle a des poules tout les matins elle ramasse les oeufs et les mets dans des boites.
+
+Quand elle a fini de ramasser les oeufs elle appelle sont fils en lui donnant le nombre d'oeuf, il doit calculer le nombre de boites de 6 oeufs et le nombre d'oeufs restants.
+
+Aidons le avec // qui est la division entière et % (opérateur modulo) qui calcul le reste de la division entière. 
+
+
+
+
+==
+
+code==
+nbreoeufs = int( input("saississez le nombre d'oeufs :") )
+
+print("Il faut "+ XXX + "boites.")
+print("il restera "+ YYY + "oeufs.") 
+
+
+==
+
+inputgenerator==
+from random import randint
+
+print(randint(10,100))
+==
+soluce==
+nbreoeufs = int( input("saississez le nombre d'oeufs :") )
+
+print("Il faut "+ nbroeufs // 6 + "boites.")
+print("il restera "+ nbroeufs % 6 + "oeufs.") 
+
+==
+
