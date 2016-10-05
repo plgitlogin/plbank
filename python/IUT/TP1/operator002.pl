d7c0d30e259ecae1bb4d62c49d63e4db043ad81e
Dominique <dominique.revuz@gmail.com>
2016-10-05 12:08:39 +0200

    work

---
+++ python/IUT/TP1/operator002.pl
@@ -0,0 +1,46 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= Calcul d'intérêt
+title= Operateurs mutliplicatifs 
+
+tag= print|input|operator.mod|operator.floordiv
+template=/python/IUT/template
+text==
+
+# Operator ** / * + - 
+
+Vous devez emprunter pour construire votre projet de vie (maison voiture etc.).
+
+Vous voulez savoir l'importance du taux d'interet et du nombre d'année sur vos mensualités voici la formule utilisée par les banques pour calculer le montant d'une mensualité:
+
+	<img src="https://latex.codecogs.com/gif.latex?\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" title="\frac{k*\frac{t}{12}}{1-\left ( 1+\frac{t}{12} \right )^{-n}}" />
+
+En utilisant cette formule calculez et afficher la mensualité si le capital emprunté est k=150000, la durée de l'emprunt est n=144 mois (douze ans), et  le taux d'intéret est t=0.023 soit 2,3 pourcent.
+
+<a href="https://www.codecogs.com/eqnedit.php?latex=\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" title="\frac{k*\frac{t}{12}}{1-\left ( 1+\frac{t}{12} \right )^{-n}}" /></a>
+
+Opérateur x ** y l'exponentiation  x à la puissance y.
+Opérateur x / y la vrai division  x divisé par y.
+Opérateur x * y la multiplication x multiplier par y.
+Pensez à la précédence des opérateurs en utilisant des paranthèses pour être sur de l'ordre des calculs.
+
+==
+
+code==
+k=150000
+n=144
+t=0.023
+m= ? # Ecrivez la formule en python
+
+print("Montant de la mensualitée=",int(m))
+
+==
+
+# 
+
+
+
+expectedoutput==
+Montant de la mensualitée= 1193
+==
+
