b9358d5af7c8f861002db50cd53a6cd6649bb8ff
Dominique <dominique.revuz@gmail.com>
2016-10-06 16:56:45 +0200

    work

---
+++ python/IUT/TP1/errortype.pl
@@ -0,0 +1,24 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= Erreurs de Types 
+title=  Erreur de Types 001
+tag=TypeError  # N'oubliez pas de remplir ce champs svp
+template=/python/IUT/template
+
+text==
+
+#  Erreurs de Types
+Dans le code suivant il y a une erreur de compilation qui est due à un opérateur qui est utilisé avec des arguments qui sont de types incompatibles.
+
+
+==
+
+
+code==
+# Il faut transformer l'entier int en chaine str pour que l'opérateur + fonctionne.
+print(" Nombre de jours dans une semaine "+7)
+# 
+
+
+# Choisir pltest ou soluce ou expectedoutput
+
