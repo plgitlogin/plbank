9291b15777cfdc0979d225b5026fe177ee820506
Dominique <dominique.revuz@gmail.com>
2016-10-06 17:45:29 +0200

    work

--- python/IUT/TP1/operator002.pl
+++ python/IUT/TP1/operator002.pl
@@ -13,11 +13,11 @@ Vous devez emprunter pour construire votre projet de vie (maison voiture etc.).
 
 Vous voulez savoir l'importance du taux d'interet et du nombre d'année sur vos mensualités voici la formule utilisée par les banques pour calculer le montant d'une mensualité:
 
-	<img src="https://latex.codecogs.com/gif.latex?\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" title="\frac{k*\frac{t}{12}}{1-\left ( 1+\frac{t}{12} \right )^{-n}}" />
+<a href="https://www.codecogs.com/eqnedit.php?latex=\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" title="\frac{k*\frac{t}{12}}{1-\left ( 1+\frac{t}{12} \right )^{-n}}" /></a>
+
 
 En utilisant cette formule calculez et afficher la mensualité si le capital emprunté est k=150000, la durée de l'emprunt est n=144 mois (douze ans), et  le taux d'intéret est t=0.023 soit 2,3 pourcent.
 
-<a href="https://www.codecogs.com/eqnedit.php?latex=\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\frac{k*\frac{t}{12}}{1-\left&space;(&space;1&plus;\frac{t}{12}&space;\right&space;)^{-n}}" title="\frac{k*\frac{t}{12}}{1-\left ( 1+\frac{t}{12} \right )^{-n}}" /></a>
 
 Opérateur x ** y l'exponentiation  x à la puissance y.
 Opérateur x / y la vrai division  x divisé par y.
