c4ec71bde1faaa1655a1800452fcccbe4e1560c4
REVUZ Dominique <drevuz@localhost.localdomain>
2016-10-13 16:15:56 +0200

    work

---
+++ python/IUT/TP2/function001.pl
@@ -0,0 +1,27 @@
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+name= 
+title= 
+tag= 
+template=/python/IUT/template
+text==
+
+# Fonctions 
+
+Ecrire une fonction **bob** qui retourne la valeur entière 1238.
+
+==
+
+code==
+
+# Fin du code, n'écrivez pas de code après cette ligne s'il vous plait !
+# L'équipe PL
+==
+
+pltest==
+>>> bob()
+1238
+>>> bobo()==1238
+True
+==
+
