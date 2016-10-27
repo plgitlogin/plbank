601c1ce2d7cc9410426a5778cc31f32b0fcc994f
Dominique <dominique.revuz@gmail.com>
2016-10-27 14:55:55 +0200

    pltest-creat

--- python/IUT/template.pl
+++ python/IUT/template.pl
@@ -1,15 +1,9 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
-author=Dominique Revuz 
+
 name=  python/iut/template.pl
-title= Ceci est un template ce n'est pas un exercice 
-
-
-
-files=@ /pysrc/src/utils.py
-
 
 grader==
 from utils import grade
 grade()
 ==
-
+files=@/pysrc/src/utils.py
