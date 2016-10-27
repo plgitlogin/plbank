fda14b2f145206caee5d9c5225c85add10701adc
Dominique <dominique.revuz@gmail.com>
2016-10-27 15:00:21 +0200

    work

--- python/function/pom.pl
+++ python/function/pom.pl
@@ -3,7 +3,7 @@ author=Dominique Revuz
 name=  pom.pl
 title=  Pom Pom Pom Pom  # N'oubliez pas de remplir ce champs svp
 tag=  # N'oubliez pas de remplir ce champs svp
-template=/python/exemple/autogradertemplate
+template=/python/IUT/template.pl
 text==
 
 Ecrire une fonction **pom** qui prend un parametre entier n et qui affiche
