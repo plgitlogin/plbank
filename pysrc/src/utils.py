bd8cfff1e87eaa0fed5a17ea6793e7fcae3bb35f
Dominique <dominique.revuz@gmail.com>
2016-10-27 15:52:09 +0200

    work

--- pysrc/src/utils.py
+++ pysrc/src/utils.py
@@ -365,14 +365,14 @@ def grade():
 			message += "\n\nsortie optenue:\n\n" + pldecode( d['stdout'])
 			erreurdexecution(message)
 	elif 'pltest' in pld:
-		f=open("student.py","a")
-		print("\"\"\"\n",file=f)
-		print(pld["pltest"],file=f)
-		print("\"\"\"\n",file=f)
-		d=exectojson(['-m','doctest','student.py'])
-		# copier à la fin de student.py le doctest puis lancer la commande
-		# python3 -m doctest student.py
-		erreurdexecution(d['stdout'])
+		with f=open("student.py","a") as f:
+			print("\"\"\"\n",file=f)
+			print(pld["pltest"],file=f)
+			print("\"\"\"\n",file=f)
+			d=exectojson(['-m','doctest','student.py'])
+			# copier à la fin de student.py le doctest puis lancer la commande
+			# python3 -m doctest student.py
+			erreurdexecution(d['stdout'])
 	elif 'soluce' in pld:
 # il faut pour tous les input* verifier que l'execution de student celle de soluce
 # ou bien faire inputgeneratorcalls appels à inputgenerator et verifier la même chose
