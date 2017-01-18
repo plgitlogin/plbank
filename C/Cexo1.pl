21a860a541405e61352ea4c0f4c8d09a947d0c58
Dominique <dominique.revuz@gmail.com>
2017-01-18 10:28:01 +0100

    nouveau Cexo1.pl

---
+++ C/Cexo1.pl
@@ -0,0 +1,29 @@
+# Copyright 2016 DR
+
+
+title=Premier programme en C
+tag=output
+template=/C/template
+
+text==
+# Premières instuctions 
+Ecrire une suite d'instruction qui affiche *Hello
+World!* sur la sortie standard ainsi qu'un retourne à la ligne '\n'.
+
+## remarques
+Pour ceux qui ont déjà fait du il n'y pas besoin de main() ici.
+Juste les lines d'instructions nécesaires.
+
+
+==
+
+code==
+/* votre code ici... */
+==
+
+instruction=defined
+
+expectedoutput==
+Hello World!
+==
+
