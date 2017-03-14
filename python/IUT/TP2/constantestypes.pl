b498fc7ca802e6a9cea453161c2789413fa26bf9
plgitlogin <dominique.revuz@univ-mlv.fr>
2017-03-14 18:07:05 +0100

    initial TP2

---
+++ python/IUT/TP2/constantestypes.pl
@@ -0,0 +1,23 @@
+# Copyright 2017 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz
+name=constantestypes.pl
+title= Variables et Types
+tag= int|str|type|
+template=/python/template
+text==
+
+
+# Constantes et types
+
+
+# Variables et Types
+
+
+
+
+
+
+==
+code==
+# code intiale de l'editeur
+==
