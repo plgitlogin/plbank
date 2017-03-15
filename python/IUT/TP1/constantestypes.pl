a6f3af90dff0bccf2d8aad5dcbb3acd64523ae5c
plgitlogin <dominique.revuz@univ-mlv.fr>
2017-03-15 15:01:54 +0100

    TP1

---
+++ python/IUT/TP1/constantestypes.pl
@@ -0,0 +1,31 @@
+# Copyright 2017 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz
+name=constantestypes.pl
+title= Variables et Types
+tag= int|str|type|constant
+template=/python/template
+text==
+
+
+# Constantes et types
+
+Nous utilisons dans la vie de tout les jours de nombreuses constantes:
+3, "demain", 15h30, "Mille-Feuilles".
+A la boulangerie nous échangeons avec la boulangère:
+
+	Je voudrais réserver *3* *Mille-Feuilles* pour *demain*.
+	Oui. Ils seront près à *15h30*.
+
+Si nous regardons ces différentes constantes nous constatons qu'elles ne sont pas échangeables dans la phrase.
+Mais il est possible de les changer en les remplacant par une autre constante du MEME TYPE.
+
+	Je
+
+
+
+
+
+==
+code==
+# code intiale de l'editeur
+==
