d8e58e87d57f2587be88570b7ed381d0846f876e
Dominique <dominique.revuz@gmail.com>
2017-03-29 16:13:02 +0200

    pgrader template prints

--- python/0PLG/template.pl
+++ python/0PLG/template.pl
@@ -1,11 +1,13 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 
+# using the new plgrader 
+
 name=  python/0PLG/template.pl
 
 grader==
 from plgrader import Grader
 g=Grader()
-g.grade()
+print(g.grade())
 ==
 files=@/pysrc/src/plgrader.py
 files=@/pysrc/src/feedback.py
