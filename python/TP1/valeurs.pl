a40dfe8e3afa7faf207443faa4772710a276e4e3
Dominique <dominique.revuz@gmail.com>
2017-04-28 12:04:42 +0200

    "plcheck commit"

--- python/TP1/valeurs.pl
+++ python/TP1/valeurs.pl
@@ -1,32 +1,48 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
 name= Quelques Constantes
-title=   # N'oubliez pas de remplir ce champs svp
-tag=  # N'oubliez pas de remplir ce champs svp
-template=/python/exemple/autogradertemplate
+title= Calculs avec des constantes   # N'oubliez pas de remplir ce champs svp
+tag=constant|print|int # N'oubliez pas de remplir ce champs svp
+#template=/python/exemple/autogradertemplate
+template=/python/0PLG/template
 text==
 
+Ouvrez un terminal (vu en tp2 ISI),
+dans le terminal lancer la commande //python3//.
 
-Sachant que l'unité de distance la plus petite est 
-$$ 1,616199 10^{−35}$$ metres soit la Longueur de Planck
-et que $$1,3 10^{28}$$ metres distance du plus loingtain quasar.  
+Vous obtenez quelque chose comme :
+	Python 3.5.1 |Anaconda 2.4.1 (x86_64)| (default, Dec  7 2015, 11:24:55) 
+	[GCC 4.2.1 (Apple Inc. build 5577)] on darwin
+	Type "help", "copyright", "credits" or "license" for more information.
+	>>> 
 
-Dans un interpréteur python essayez le code suivant :
+Les trois caractères //>>>// veullent sont une invite de commande il ne vous reste plus qu'a interagir avec ce python.
 
 
-	PI=3.14156 # en americain  
+
+Dans cet interpréteur python essayez le code suivant :
+
+
+	PI=3.14156 # Une constante connue   
 	km= 10**3 # un kilometre c'est 1000 metres
 	a=1 000 000 000 # erreur de syntaxe
 	a=1.000.000.000 # erreur de syntaxe
 	a=1,000,000,000 # 4 valeurs !!!!
 	a
-	a=10^9 
-	a # surprise 
 	a=10**9
 	a
+	a=10^9 
+	a # surprise 
 
 
-Puis répondez à l'exercice en calculant la distance de ce quasar en longueurs de Planck et en affichant celle ci la sur une ligne en notation mathématique (affichage par défaut). Puis sous forme d'un entier en utilisant:
+Puis revenez dans le elearning et répondez à l'exercice:
+
+Sachant que l'unité de distance la plus petite est 
+	$$ 1,616199 10^{−35}$$ metres soit la Longueur de Planck
+	et que $$1,3 10^{28}$$ metres est la distance du plus lointain quasar.  
+
+en calculant la distance de ce quasar en longueurs de Planck et en affichant celle ci la sur une ligne en notation mathématique (affichage par défaut). Puis sous forme d'un entier en utilisant:
+
 
 	print(int(ladistance))
 
