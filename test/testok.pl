9bf1155d961788cb0eea084aafe93ecc3a7dd14a
Dominique <dominique.revuz@gmail.com>
2017-05-04 16:59:46 +0200

    big sort

--- test/testok.pl
+++ test/testok.pl
@@ -1,8 +1,8 @@
 # Copyright 2017 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
 name=testok.pl
-title=   # N'oubliez pas de remplir ce champs svp
-tag=  # N'oubliez pas de remplir ce champs svp
+title=  Testeur de la plateforme 
+tag=nil
 template=/python/test/template.pl
 
 text==
@@ -29,5 +29,19 @@ grader==
 		import pldoctest
 	except ImportError as ie:
 		print("Problème le fichier ",ie.name,"est abscent de l'environnement d'execution")
+>>>import os
+>>> os.path.exists("student.py")
+True
+>>> os.path.exists("pl.json")
+True
+
+# essailler de contacter l'extérieur
+#try:
+	#import urllib
+	##params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+	#f = urllib.urlopen("http://www.python.org/")
+	#print("Problème le fichier est abscent de l'environnement d'execution")
+#except ImportError as ie:
+	#print(" urllib n'est pas installée")
 
 ==
