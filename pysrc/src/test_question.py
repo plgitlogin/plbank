2462b58594ca647399564103a574a8b2d208f30e
Dominique <dominique.revuz@gmail.com>
2017-05-04 16:59:46 +0200

    big sort

--- pysrc/src/test_question.py
+++ pysrc/src/test_question.py
@@ -7,43 +7,49 @@
 #  
 
 import question
+import plrequest
+import os
+
 
 def mysetup(text, filename="qtest.pl"):
     with open(filename,"w") as qt:
         print(text,file=qt)
 
+def myteardown( filename="qtest.pl"):
+	os.remove(filename)
+
 def printdico(d):
-	print("{",end="")
-	for k in d.keys():
-		if type(d[k]) == str :
-			print("\""+k+"\" : \""+d[k],end="\",")
-		elif type(d[k]) == list :
-			print("["+",".join(d[k])+"]",end=",")
-		elif type(d[k]) == dict :
-			printdico(d[k])
-			print(end=",")
-		else:
-			print("Type étrange ")
-	print("}",end="")
+    print("{",end="")
+    for k in d.keys():
+        if type(d[k]) == str :
+            print("\""+k+"\" : \""+d[k],end="\",")
+        elif type(d[k]) == list :
+            print("["+",".join(d[k])+"]",end=",")
+        elif type(d[k]) == dict :
+            printdico(d[k])
+            print(end=",")
+        else:
+            print("Type étrange ")
+    print("}",end="")
 
 def dicotopl(d):
-	for k in d.keys():
-		if type(d[k]) == str :
-			if not "\n" in d[k]:
-				print(k+"= "+d[k])
-			else:
-				print(k+"==")
-				for line in d[k].split("\n"):
-					print(line)
-				print("==")
-		elif type(d[k]) == list :
-			print("["+",".join(d[k])+"]",end=",")
-		elif type(d[k]) == dict :
-			for z in d[k]:
-				print("files=@ "+z)
-			print()
-		else:
-			raise ValueError("Type étrange ")
+        for k in d.keys():
+            if type(d[k]) == str :
+                if not "\n" in d[k]:
+                    print(k+"= "+d[k])
+                else:
+                    print(k+"==")
+                for line in d[k].split("\n"):
+                    print(line)
+                    print("==")
+            elif type(d[k]) == list :
+                print("["+",".join(d[k])+"]",end=",")
+            elif type(d[k]) == dict :
+                for z in d[k]:
+                    print("files=@ "+z)
+                print()
+            else:
+                raise ValueError("Type étrange ")
 
 
 
@@ -55,7 +61,8 @@ def test_Question_load_simple():
     """
     mysetup("bob== hhh\ndes truc\n==\naa=bb\nccccccc=k\nbxb==xx\n\n\nxxx yyyy\n# des trucs \n==\n# doit pas apparaitre")
     q=question.Question("qtest.pl",root=".")
-    assert json.loads(q.json) == {"bob": "des truc[]", "ccccccc": "k", "bxb": "[][]xxx yyyy[]# des trucs []", "aa": "bb",'url': 'qtest.pl',}
+    myteardown()
+    assert json.loads(q.json) == {"bob": "des truc\n", "ccccccc": "k", "bxb": "\n\nxxx yyyy\n# des trucs \n", "aa": "bb",'url': 'qtest.pl',}
 
 
 def test_question_load_withfiles():
@@ -68,6 +75,10 @@ def test_question_load_withfiles():
     mysetup("lefichier grader.py","fortest3.py")
     
     q=question.Question("qtest.pl",root=".")
+    myteardown()
+    myteardown('fortest1.py')
+    myteardown('fortest2.py')
+    myteardown('fortest3.py')
     assert json.loads(q.json) == {"a": "b", "basefiles": {"fortest1.py": "lefichier pout les test 1 \n nom unix fotest1.py\n", "fortest3.py": "lefichier grader.py\n", "fortest2.py": "lefichier g\n\n\n\n\n\n\n\n\n\n\n\ny\n"},'url': 'qtest.pl'}
 
 def test_question_save_to_dir():
@@ -76,20 +87,31 @@ def test_question_save_to_dir():
     mysetup("lefichier g\n\n\n\n\n\n\n\n\n\n\n\ny","fortest2.py")
     mysetup("lefichier grader.py","fortest3.py")
     q=question.Question("gommo.pl",root=".")
-    dirname = q.createDir("print(4)")
-    print(type(dirname))
-    assert len(dirname.listdir()) == 1
+    myteardown("gommo.pl")
+    myteardown('fortest1.py')
+    myteardown('fortest2.py')
+    myteardown('fortest3.py')
+    dirname = q.createdir("print(4)")
+    assert len([x for x in dirname.iterdir()]) == 5
+    from shutil import rmtree
+    rmtree(str(dirname))
 
-import request
 
 def qforsandbox():
-	mysetup("autor=dr\nsoluce=print(4)\n#\nbob==\nimport json\n d={ \"success\": True , \"errormessages\" : \"\",\"feedback\": \"Yesssss\", \"other\": \"\",\"error\":\"\",\"execution\": \"\",\"grade\":\"1\"}\nprint(json.dump(d))\n==\nexpectedoutput==\n4\n==\n","testSanbox.pl")
-	return question.Question("testSanbox.pl",root=".")
+    mysetup("autor=dr\nsoluce=print(4)\n#\ngrader==\n\nimport json\nd={ \"success\": True , \"errormessages\" : \"\",\"feedback\": \"Yesssss\", \"other\": \"\",\"error\":\"\",\"execution\": \"\",\"grade\":\"1\"}\nprint(json.dumps(d))\n==\nexpectedoutput==\n4\n==\n","testSandbox.pl")
+    q =question.Question("testSanbox.pl",root=".")
+    myteardown("testSandbox.pl")
+    q.dir  = q.createdir("print(5)\n")
+    return q
 
 def test_SanboxSession():
-	q=qforsandbox()
-	s= request.SanboxSession(q,"http://pl-sandbox.u-pem.fr/?action=version")
-	s.call()
-	print(s.answer.text)
-	assert s.answer.text == ""
-	assert 0
+    q=qforsandbox()
+    s= plrequest.SanboxSession(q,"http://pl-sandbox-test.u-pem.fr/?action=version",studentfile="print(4)")
+    assert s.answer.text == '{"version":"0.5"}'
+
+
+def test_SanboxSession2():
+    q=qforsandbox()
+    s= plrequest.SanboxSession(q,"http://pl-sandbox-test.u-pem.fr/?action=execute",studentfile="print(4)")
+    print(s.answer.text)
+    assert json.loads(s.answer.text) == {"platform_error":[],"grade":{"errormessages":"","execution":"","feedback":"Yesssss","error":"","success":True,"other":"","grade":"1"}}
