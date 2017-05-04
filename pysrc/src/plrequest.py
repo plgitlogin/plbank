76bbc028deb7c51eb1a6da0dd570804757b37d41
Dominique <dominique.revuz@gmail.com>
2017-05-04 15:16:23 +0200

    request

--- pysrc/src/plrequest.py
+++ pysrc/src/plrequest.py
@@ -8,33 +8,69 @@
 
 __doc__ = """
 
-	Ce fichier a pour objectif de gérer les comunications
-	avec la sandbox.
-	"""
+    Ce fichier a pour objectif de gérer les communications
+    avec la sandbox.
+    """
 
 
-import plrequest
+import requests
 import question
+import zipfile
+import pathlib
 
+import cd
+import subprocess
+import hashlib
+
+def pllogdata(user,sha1,studentfile=None,mode="try",url = "http://127.0.0.1:9090"):
+    if studentfile == None:
+        mode= "start"
+    geturl=url+"/add/tt"
+    posturl=url+"/add/xx"
+    try:
+        csrftoken = requests.get(geturl).cookies['csrftoken']
+        header = {'X-CSRFToken': csrftoken}
+        cookies = {'csrftoken': csrftoken}
+        r=requests.post(posturl,headers=header, cookies=cookies,data={"user":user,"code":studentfile,"mode":mode,"sha1":sha1})
+    except Exception as e:
+        print(" Berk can't access pldata",e) # don't dye for this
+        r=None
+    return r
 
 class SanboxSession:
-	def __init__(self,question,url):
-		self.question = question
-		self.url = url
+    def __init__(self,question,url,studentfile):
+        self.question = question
+        self.url = url
+        self.studentfile = studentfile
+        self.call(studentfile)
 
-	def createEnvFile(self):
-		from shutil import rmtree
-		rmtree('/tmp/env/')
-		p=Path('/tmp/env/')
-		p.mkdir()
-		for name,
-
-
-
-	def call(self,studentfile):
-		self.files = {'environment': open('/tmp/env.zip', 'rb'),'student.py':studentfile}
-		self.answer = requests.post(self.url,data=self.question.dico,files=self.files,timeout=0.1)
-
+    def createEnvZipRun(self):
+        from shutil import rmtree
+        rmtree('/tmp/env/', ignore_errors=True)
+        p=pathlib.Path('/tmp/env/')
+        p=self.question.createdir(self.studentfile)
+        self.zipname = str(p.resolve() /  "env.zip")
+        with cd.cd(str(p)) :
+            subprocess.run(['zip','-qjr','env.zip','.'])
+            
 
 
+
+    def checkgrader(self):
+        self.question.checkgrader()
+
+    def call(self,studentfile):
+        self.createEnvZipRun()
+        self.checkgrader()
+        mn=hashlib.sha1()
+        zipvalue=open(self.zipname, 'rb').read()
+        mn.update(zipvalue[:])
+        self.hashvalue = mn.hexdigest()
+        self.files = {'environment': zipvalue,
+            'student.py':studentfile,
+            "grader.py":self.question.dico['grader'],
+            'hashvalue':self.hashvalue}
+        pllogdata(-1,self.hashvalue,studentfile)
+        self.answer = requests.post(self.url,data=self.question.dico,files=self.files,timeout=1000)
+        return self.answer
 
