7ac24a4926c639541fc82547fca391a6fd917d43
Dominique <dominique.revuz@gmail.com>
2017-05-04 16:59:46 +0200

    big sort

--- test/allwayscorrect.pl
+++ test/allwayscorrect.pl
@@ -25,6 +25,8 @@ An [example link](http://example.com/ "Optional Title") in a sentence.
 ==
 
 grader==
-print('{"execution": "*italic* **bold** ***bold italic***", "other": "", "errormessages": "", "success": true, "feedback": "ceci n\'est pas un exercice doit \\u00eatre utilis\\u00e9 uniquement comme \\u00e9lement de test"}')
+print('{"execution": "*italic* **bold** ***bold italic***", "other": "", "errormessages": "", "success": true, "feedback": "ceci n\'est pas un exercice doit \\u00eatre utilis\\u00e9 uniquement comme \\u00e9lement de test\\n*italic* **bold** ***bold italic***"}')
 ==
 
+
+# n'utilise pas 0PLG
