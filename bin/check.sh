00dc92adcb81f9d2378cb122626d1a2561989fdb
rbilal <rbilal@localhost.localdomain>
2017-06-15 15:32:46 +0200

    Check fini, tout les pl de all.pltp sont fonctionnel

---
+++ bin/check.sh
@@ -0,0 +1,3 @@
+check_aux.sh $*
+
+git push
--- bin/check_aux.sh
+++ bin/check_aux.sh
@@ -7,9 +7,9 @@ do
             continue
     fi
 
-    touch $rootdir/../../server/serverpl/tmp_test.py
+    touch $rootdir/../../server/serverpl/test_tmp.py
 
-    cat > $rootdir/../../server/serverpl/tmp_test.py <<EOF
+    cat > $rootdir/../../server/serverpl/test_tmp.py <<EOF
 from pysrc.newcheck import checkplfile, checkpltpfile
 from gitload.loader import loadPL, loadPLTP
 from gitload.models import Repository
@@ -25,7 +25,7 @@ EOF
     if [ ${i#*.} = "pl" ]
     # Writing testpl in the file 
     then
-        cat >> $rootdir/../../server/serverpl/tmp_test.py <<EOF
+        cat >> $rootdir/../../server/serverpl/test_tmp.py <<EOF
 @pytest.mark.django_db
 def test_checkplfile():
     if ":" in "$i":
@@ -35,32 +35,32 @@ def test_checkplfile():
         
     assert(checkplfile('$i', r, sandboxurl = 'http://127.0.0.1:8000/sandbox/?action=execute'))
 EOF
-        pytest $rootdir/../../server/serverpl/tmp_test.py
+        pytest $rootdir/../../server/serverpl/test_tmp.py
         # Test was collected and passed successfully
         if [ $? -eq 0 ]
         then
             git commit -m " newcheck commit " $i
             echo "commit "$i
-            rm $rootdir/../../server/serverpl/tmp_test.py
+            rm $rootdir/../../server/serverpl/test_tmp.py
         fi
     elif [ ${i#*.} = "pltp" ]
     #Writing testpltp in the file 
     then
-        cat >> $rootdir/../../server/serverpl/tmp_test.py <<EOF
+        cat >> $rootdir/../../server/serverpl/test_tmp.py <<EOF
 @pytest.mark.django_db
 def test_checkpltpfile():
     r = Repository(name=os.path.basename('$rootdir'),url='elsewhere',version=1)
     assert(checkpltpfile('$i', r, sandboxurl = 'http://127.0.0.1:8000/sandbox/?action=execute'))
 EOF
-        pytest $rootdir/../../server/serverpl/tmp_test.py
+        pytest $rootdir/../../server/serverpl/test_tmp.py
         # Test was collected and passed successfully
         if [ $? -eq 0 ]
         then
             pltp=$i
-            rm $rootdir/../../server/serverpl/tmp_test.py
+            rm $rootdir/../../server/serverpl/test_tmp.py
             echo "relance"
             #Get all the pl in the current pltp removing the "@ " and apply the command on them 
-            grep "^@" $i  | sed -e "s/^@ /*//" | xargs newplcheck.sh
+            grep "^@" $i  | sed -e "s/^@ \/*//" | xargs check_aux.sh
             echo "commit "$i
             git commit -m " newcheck commit " $i
         elif [ $? -eq 1 ]
@@ -69,10 +69,8 @@ EOF
         fi
     else
         echo $i" n'est pas un fichier conforme" 
-        rm $rootdir/../../server/serverpl/tmp_test.py
+        rm $rootdir/../../server/serverpl/test_tmp.py
         continue
     fi
 
 done
-
-git push
