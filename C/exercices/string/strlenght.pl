728f2ee9abef73f2b5a15fde80b67d38350c2951
Nicolas Borie <nicolas.borie@u-pem.fr>
2017-06-27 13:19:26 +0200

    tests on second C grader

--- C/exercices/string/strlenght.pl
+++ C/exercices/string/strlenght.pl
@@ -63,5 +63,6 @@ files=@/C/template/graderC.py
 
 grader==
 from graderC import grade_argcmd_stdin_cmp_soluce
-grade_argcmd_stdin_cmp_soluce( {"Petit mot": [" mot","", True], "Un mot": [" Bonjour","", True], "Deux mots" : [" 'ca va?' ","", True], "Mot vide" : [" ''","", True], "Long mot": [" 'Anti-constitutionnellement'","", True] }, "-Wall -ansi")
+tests = {"Petit mot": [" mot","", True], "Un mot": [" Bonjour","", True], "Deux mots" : [" 'ca va?' ","", True], "Mot vide" : [" ''","", True], "Long mot": [" 'Anti-constitutionnellement'","", True] }
+grade_argcmd_stdin_cmp_soluce(tests=tests, flags="-Wall -ansi")
 ==
