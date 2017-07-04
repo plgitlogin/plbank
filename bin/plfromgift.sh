114dcad23378f669fbd5700608814f429dd47570
igm <igm@dorme.univ-mlv.fr>
2017-07-04 15:17:48 +0200

    modif du bash pour la dest

--- bin/plfromgift.sh
+++ bin/plfromgift.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 rootdir=$(git rev-parse --show-toplevel)
+first_it=1
 for i in $@
 do
-    python $rootdir/server/serverpl/gitload/pltpfromgift.py $i
+    if [ $first_it == 1 ]
+    then
+        path=$i
+        first_it=0
+        continue    
+    fi
+    python $rootdir/server/serverpl/gitload/pltpfromgift.py $path $i
 done
---
+++ gift/description/You_can_use_your_pen.pl
@@ -0,0 +1,13 @@
+text==
+You can use your pencil and paper for these next math questions.
+==
+title==
+You can use your pen
+==
+type=description
+evaluator==
+def evaluator(None): return True, 'Merci pour votre lecture'
+==
+name==
+You_can_use_your_pen
+==
